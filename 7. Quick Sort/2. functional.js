e0488d33b25f46d4d6b1d436db421ae688afdf01
rayjune <ray_june@qq.com>
2018-03-26 16:50:43 +0800

    add 2.functional to Quisk Sort

---
+++ 7. Quick Sort/2. functional.js
@@ -0,0 +1,17 @@
+function quickSort(arr) {
+  const pivot = arr.shift();
+  const left = [];
+  const right = [];
+
+  if (arr.length < 2) { return arr; }
+
+  arr.forEach((element) => {
+    element < pivot ? left.push(element) : right.push(element);
+  });
+
+  return quickSort(left).concat([pivot], quickSort(right));
+}
+
+// test
+const arr = [91, 60, 96, 7, 35, 65, 10, 65, 9, 30, 20, 31, 77, 81, 24];
+console.log(quickSort(arr));
